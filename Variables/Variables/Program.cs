955905762a0fe2f2ed241922aeeb7e6de54b8046
Gustavo Robles <smickher@gmail.com>
2021-10-05 23:43:34 -0500

    New codes
    goo

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables y subindices
+            int variableA = 2;
+            Console.WriteLine("Variable de tipo entero tiene el valor: "+variableA);
+
+            String nombre = "Gustavo Robles";
+            Console.WriteLine("El contenido de la cadena es: " +nombre);
+
+            bool ganador = false;
+            Console.WriteLine("La variable tipo boolean es: {0}", ganador);
+
+            //imprimir 2 variables en la misma linea
+            Console.WriteLine("El valor del entero es: {0}, El valor de la Cadena de Texto es: {1}, El valor de la variable Boleana es: {2}",variableA,nombre,ganador);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ hola_mundo/hola_mundo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace hola_mundo//es un bloque de codigo que ejecuta un programa y tenemos acceso con using.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //varibles TipodeDato NombreDeLaVariable Inicializacion
+            int numero = 0;
+            String cadena = "Gustavo Robles";
+            Console.Write("El participante "+cadena+" Es el numero: "+numero);
+
+            /*Console.Write("Hello world!\n");
+            Console.WriteLine("Welcome to C Sharp");*/
+            Console.ReadKey();
+        }
+    }
+}
