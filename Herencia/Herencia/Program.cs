8b11f3581b94947fa37fc3a0c79c58b968a22e69
Gustavo Robles <smickher@gmail.com>
2021-10-09 16:27:24 -0500

    herencia

---
+++ Herencia/Herencia/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automovil auto1 = new Automovil();
+            Avion avion1 = new Avion();
+
+            auto1.muestraprecio();
+            avion1.muestraprecio();
+            Console.ReadKey();
+        }
+    }
+
+    class Transporte
+    {
+        //atributos
+        protected string Nombre;
+        protected int precio;
+        protected int NumPasajeros;
+
+    }
+
+    class Automovil: Transporte
+    {
+        //atributos
+        //metodo
+        public void muestraprecio()
+        {
+            precio = 500;
+            Console.WriteLine("El precio de este transporte es: ${0}", precio);
+        }
+    }
+    class Avion: Transporte
+    {
+        //atributos
+        //metodo
+        public void muestraprecio()
+        {
+            precio = 700;
+            Console.WriteLine("El precio de este transporte es: ${0}", precio);
+        }
+    }
+}
